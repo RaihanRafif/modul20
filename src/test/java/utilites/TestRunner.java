d799c28a5a1a055f752e608dfcd3593b27699030
Raihan Rafif <raihanrafif200095@gmail.com>
2023-12-16 13:15:38 +0700

    fixing test runner code

--- src/test/java/utilites/TestRunner.java
+++ src/test/java/utilites/TestRunner.java
@@ -11,36 +11,13 @@ import org.testng.xml.XmlTest;
 public class TestRunner {
     public static void main(String[] args) {
         TestNG testng = new TestNG();
-        XmlSuite suite = new XmlSuite();
-        suite.setName("Test API");
 
-        // Add a list of XmlTests to the XmlSuite
-        List<XmlTest> tests = new ArrayList<>();
+        List<String> suites = new ArrayList<String>();
 
-        // Define the test names based on your testng.xml
-        String[] testNames = { "Test Positive", "Test Negative","Test Boundary" };
+        suites.add("src/test/resources/testng.xml");
 
-
-        for (String testName : testNames) {
-            XmlTest xmlTest = new XmlTest(suite);
-            xmlTest.setName(testName);
-
-            // Add the classes to the XmlTest
-            List<XmlClass> xmlClasses = new ArrayList<>();
-            xmlClasses.add(new XmlClass("tests.APITest"));
-            xmlTest.setClasses(xmlClasses);
-
-            tests.add(xmlTest);
-        }
-
-        // Add the suite to TestNG
-        List<XmlSuite> suites = new ArrayList<>();
-        suites.add(suite);
-
-
-        testng.setXmlSuites(suites);
-
-        // Run the tests
+        testng.setTestSuites(suites);
         testng.run();
+
     }
 }
